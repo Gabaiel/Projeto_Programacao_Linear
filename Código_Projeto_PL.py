79840a4c536146069ba97db809adb198515359c5
Gabriel <ga6riel06@gmail.com>
2025-10-23 22:07:31 -0300

    main

--- Código_Projeto_PL.py
+++ Código_Projeto_PL.py
@@ -11,6 +11,16 @@
 # • x: vetor das vari ́aveis de decis ̃ao, que  ́e a solu ̧c ̃ao  ́otima do PPL;
 # • f(x): valor  ́otimo da solu ̧c ̃ao.
 
+#m, n = list(map(int, list(input("Digite o número de linhas e de colunas separadas por um espaço, respectivamente: ").split(" ")))) # Números de restrições (linhas) e variáveis (colunas)
+#c = list(map(int, list(input("Digite os custos separados por um espaço: ").split(" ")))) # Vetor dos custos
+#b = list(map(int, list(input("Digite os termos independentes separados por um espaço: ").split(" ")))) # Vetor dos recursos
+#A = [] # Matriz dos coeficientes das restrições
+#M = max(list(map(abs, c))) * 1000 # Big-M
+#
+#for i in range(0, m):
+#    row = list(map(int, list(input(f'Digite a linha {i+1} da matriz A: ').split(" "))))
+#    A.append(row)
+
 import numpy as np
 
 m, n = list(map(int, list(input().split(" ")))) # Números de restrições (linhas) e variáveis (colunas)
